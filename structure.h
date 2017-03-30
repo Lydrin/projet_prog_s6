fc97c0727e19ed29671959c482b57538fc5036f4
Anthony <gibioctet@gmail.com>
2017-03-30 23:18:59 +0200

    Ajout algo parsing + change structure

--- structure.h
+++ structure.h
@@ -1,34 +1,26 @@
-#define MAX_OEUVRES 10 
-typedef struct{
-    char dimtext[248];
-    short width;
-    short height;
-    short depth;
-    char unit[2];
-}Dimensions;
-
-typedef struct{
-    char nom[120];
-    int artistId;
-}Artist;
-
+#define MAX_OEUVRES 80000
 typedef struct{
     int id;
-    char accession_number[6];
-    Artist artiste;
-    char artisteRole[24];
-    char title[320];
-    char dateText[75];
-    char medium[120];
-    char creditLine[821];
+    char* accession_number;
+    char* artist;
+    char* artistRole;
+    int artistId;
+    char* title;
+    char* dateText;
+    char* medium;
+    char* creditLine;
     int year;
     int acquisitionYear;
-    Dimensions dimensions;
-    char inscription[14];
-    char thumbnailCopyright[984];
-    char* thumbnailURL[57];
-    char* url[134];
-}Oeuvre;
+    char* dimText;
+    int width;
+    int height;
+    int depth;
+    char* unit;
+    char* inscription;
+    char* thumbnailCopyright;
+    char* thumbnailURL;
+    char* url;
+}Oeuvre; 
 
 typedef struct{
     Oeuvre liste_oeuvre[MAX_OEUVRES];
